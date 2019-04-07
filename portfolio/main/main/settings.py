8d353ada739d367af8f96aefae460ef58376dfe3
sheawinkler <sheawinkler@gmail.com>
2019-04-07 14:34:36 -0500

    Changed database engine from SQLite to PostgreSQL

--- portfolio/main/main/settings.py
+++ portfolio/main/main/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'portfolio',
+        'USER': 'sheaportfolio',
+        'PASSWORD': '______________________',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
